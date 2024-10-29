7364b8713d07423f5a88a01ee9ae176cc006985d
emirylmz4144 <emiryilmaz41144>
2024-10-29 14:53:47 +0300

    GET isteği eklendi
    toplama endpointi altına 2 parametre alıp toplamını yazan get isteği eklendi

--- CalculatorRestApi/src/main/java/com/restapitraining/calculator/Controller/Calculator.java
+++ CalculatorRestApi/src/main/java/com/restapitraining/calculator/Controller/Calculator.java
@@ -1,9 +1,17 @@
 package com.restapitraining.calculator.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hesapmakinesi")
 public class Calculator {
+
+    //İki sayıyı toplayan GET endpointi
+    @GetMapping("/toplama")
+    public int addition(@RequestParam int number1, @RequestParam int number2) {
+        return number1 + number2;
+    }
 }
